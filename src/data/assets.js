06e7e3308636f1dcf9c25215778c8422c1c02080
Liemacs <ciubari@mail.ru>
2026-05-12 14:36:19 +0200

    Update anthem audio file path in assets.js to new location for improved organization

--- src/data/assets.js
+++ src/data/assets.js
@@ -2,4 +2,4 @@ export const placeholderImage = '/images/bg.webp'
 
 export const placeholderAvatarImage = '/images/members/unknown.webp'
 
-export const anthemAudio = '/Олений Пениc Талисман (2).mp3'
+export const anthemAudio = '/audio/vexel-anthem.mp3'
